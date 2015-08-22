7d64e57847b1bed7a38bc875faaba98a59e5e1f4
simkuring <simkuring@autistici.org>
2015-08-22 22:44:11 +0700

    add python 2 support

--- chat.py
+++ chat.py
@@ -3,7 +3,7 @@ import sys
 
 GROUP = "224.0.0.1"
 PORT = 8123
-
+PY_VERSION = sys.version[0]
 
 def main():
     help = """
@@ -29,7 +29,11 @@ def sender():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
     while(True):
-        pesan = input("Pesan : ").encode('ascii')
+        pesan = ""
+        if (PY_VERSION == '2'):
+            pesan = str(raw_input("Pesan : "))
+        else:
+            pesan = input("Pesan : ").encode('ascii')
         sock.sendto(pesan, (GROUP, PORT))
     
 
